2dbe565f1672742c272b19e2d0206210e9fee7a9
HookJabs <joshkong1@gmail.com>
2019-04-22 19:32:52 -0700

    Added filler map&list view and a toggle between them

--- app/src/main/java/com/example/wccstudentshowcase/MainActivity.java
+++ app/src/main/java/com/example/wccstudentshowcase/MainActivity.java
@@ -6,14 +6,30 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isListView = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
 
-    //
-    public void onPress(View v) {
+    //when the sign in button is pressed
+    public void onSignIn(View v) {
+        //TODO: Check if code is valid
+        setContentView(R.layout.activity_listview);
+    }
 
+    public void onToggleView(View v) {
+        //flip the state between list and map view
+        isListView = !isListView;
+        if(isListView)
+        {
+            setContentView(R.layout.activity_listview);
+        }
+        else
+        {
+            setContentView(R.layout.activity_mapview);
+        }
     }
 }
