bf54bdb340804b8b8c4e54c863fc4ed614b22dc7
Josh Kabo <joshkong1@gmail.com>
2019-04-30 11:01:56 -0700

    Updated map stuff a bit

--- app/src/main/java/com/example/wccstudentshowcase/MainActivity.java
+++ app/src/main/java/com/example/wccstudentshowcase/MainActivity.java
@@ -3,21 +3,29 @@ package com.example.wccstudentshowcase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView mapDisplay;
     private boolean isListView = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mapDisplay = findViewById();
     }
 
     //when the sign in button is pressed
     public void onSignIn(View v) {
         //TODO: Check if code is valid
         setContentView(R.layout.activity_listview);
+    }
+
+    public void onClickBuilding(View v) {
+        //TODO : read the id of the button pressed to tell what building should be shown
+
     }
 
     public void onToggleView(View v) {
